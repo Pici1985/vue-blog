4fb14be2c0bc1d8290d4b16245c40b167f514c5d
Balazs Ferencz <64424862+Pici1985@users.noreply.github.com>
2022-05-15 19:20:50 +0100

    Basics of the Composition API
    created setup() in HomeView to try out the composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click me</button>
   </div>
 </template>
 
@@ -9,14 +11,28 @@
 
 export default {
   name: 'HomeView',
+  //the values inside setup() are not reactive by default
   setup(){
     console.log('setup')
+
+    let name = 'Mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log('click')
+    }
+
+    return { 
+      name, 
+      age,
+      handleClick 
+    }
   },
-  created(){
-    console.log('created')
-  },
-  mounted(){
-    console.log('mounted')
+  //the values inside data() are reactive by default
+  data(){
+    return {
+      age: 40
+    }
   }
 }
 </script>
