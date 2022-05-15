da2b2e35c7484a0817fbbd2745cbbb11ea94d9a6
Balazs Ferencz <64424862+Pici1985@users.noreply.github.com>
2022-05-15 19:34:16 +0100

    Demonstrating refs inside setup()
    using ref inside the composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="home">
-    home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <h1>Home</h1>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
@@ -15,17 +16,22 @@ export default {
   setup(){
     console.log('setup')
 
+    const p = ref(null)
+
     let name = 'Mario'
     let age = 30
 
     const handleClick = () => {
-      console.log('click')
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'Hello gyokerek'
     }
 
     return { 
       name, 
       age,
-      handleClick 
+      handleClick,
+      p 
     }
   },
   //the values inside data() are reactive by default
