a78265e44bb4e89caf6ce89f670370743fdea38a
Balazs Ferencz <64424862+Pici1985@users.noreply.github.com>
2022-05-16 20:11:11 +0100

    LifecycleHooks inside setup()
    added onMounted,onUnmounted and onUpdated hooks

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <router-link to="/"> Home </router-link> | 
-    <router-link to="/about"> About </router-link>
+    <router-link to="/about"> About </router-link> | 
+    <router-link to="/hooks"> Hooks </router-link>
     <router-view></router-view>
   </div>
 </template>
---
+++ src/views/HooksView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Hooks</h1>
+    <p>{{ name }}</p>
+    <button @click="handleClick">Click Me</button>
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core'
+
+export default {
+  data(){
+    return {
+      name: 'Bazsi'
+    }
+  },
+  setup(){
+    onMounted(() => {
+      console.log('component mounted')
+    }),
+    onUnmounted(() => {
+      console.log('component unMounted')
+    }),
+    onUpdated(() => {
+      console.log('component updated')
+    })
+  },
+  methods: {
+    handleClick(){
+      this.name = 'Ivi'
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
