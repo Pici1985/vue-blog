7839a6f405502682ab6b8a2942c9a17edcb469b9
Balazs Ferencz <64424862+Pici1985@users.noreply.github.com>
2022-05-16 19:56:25 +0100

    Snippet and showPostBody
    created a snippet from the bady of the data and added showPostBody funct

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,13 +1,34 @@
 <template>
     <div class="post">
         <h3>{{ post.title }}</h3>
-        <p>{{ post.body }}</p>  
+        <p @click="handleClick">{{ snippet }}</p>
+        <p v-if="showPostBody">{{ post.body }}</p>
+
     </div>
 </template>
 
 <script>
+import { computed } from '@vue/runtime-core'
+
 export default {
+    data(){
+        return {
+            showPostBody: false,
+        }
+    },
     props: ['post'],
+    setup(props){
+        const snippet = computed(() => {
+            return props.post.body.substring(0, 100) + '....'
+        })
+    
+    return { snippet }
+    },
+    methods: {
+        handleClick(){
+            this.showPostBody = !this.showPostBody
+        }
+    },
 }
 </script>
 
